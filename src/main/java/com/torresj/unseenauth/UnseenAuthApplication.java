ca7c8436b4243e6ede0a11e932739fcc3a370f29
Jaime Torres <jtbenavente@gmail.com>
2022-12-27 13:52:54 +0100

    First structure

---
+++ src/main/java/com/torresj/unseenauth/UnseenAuthApplication.java
@@ -0,0 +1,13 @@
+package com.torresj.unseenauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnseenAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnseenAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torresj/unseenauth/UnseenAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torresj.unseenauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnseenAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
