78d645024072b906a4fe602c05a029ad86d96c97
Jaime Torres <jtbenavente@gmail.com>
2023-01-04 09:59:33 +0100

    Logger added

--- src/main/java/com/torresj/unseenauth/controllers/AuthController.java
+++ src/main/java/com/torresj/unseenauth/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.torresj.unseenauth.controllers;
 
+import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/auth/")
+@Log
 public class AuthController {
+
     @GetMapping("test")
     public ResponseEntity<String> test(){
         return ResponseEntity.ok("test");
